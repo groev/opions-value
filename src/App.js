a92ed7c5dd72a6e1cd9e94d90b59c805dd1afea8
Magnus Westhofen <m.westhofen@12-05.de>
2020-05-10 21:14:55 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import bs from "black-scholes";
+import { Chart } from "react-charts";
+
+import Form from "./components/Form";
 
 function App() {
+  const [result, setResult] = useState(0);
+  const [values, setValues] = useState(0);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const array = [];
+    let i;
+
+    for (i = values[2] * 365; i > 0; i--) {
+      let dataNew = values;
+      dataNew[2] = i / 365;
+      array.push([
+        values[2] - i,
+        bs.blackScholes(
+          dataNew[0],
+          dataNew[1],
+          dataNew[2],
+          dataNew[3],
+          dataNew[4],
+          dataNew[5]
+        )
+      ]);
+    }
+    console.log(array);
+    setData([array]);
+  }, [values]);
+  function calculate(e = null, values) {
+    if (e) {
+      e.preventDefault();
+    }
+    const strikedate = values.strikeDate;
+    const date1 = new Date();
+    const date2 = new Date(strikedate);
+
+    const Difference_In_Time = date2.getTime() - date1.getTime();
+    const Diff_In_Years = Difference_In_Time / (1000 * 3600 * 24) / 365;
+    const data = [
+      values.currentPrice,
+      values.strikePrice,
+      Diff_In_Years,
+
+      values.volatility / 100,
+      0.015,
+      values.type
+    ];
+
+    setValues(data);
+    const bsResult = bs.blackScholes(
+      data[0],
+      data[1],
+      data[2],
+      data[3],
+      data[4],
+      data[5]
+    );
+    console.log(values);
+    if (bsResult) {
+      setResult(bsResult);
+    }
+    return bsResult;
+  }
+  const axes = React.useMemo(
+    () => [
+      {
+        primary: true,
+        type: "linear",
+        position: "bottom"
+      },
+      { type: "linear", position: "left" }
+    ],
+    []
+  );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Option value nach Black Scholes</h1>
+      <div class="container">
+        <div>
+          <Form calculate={calculate} />
+          <div className="currentValue">{parseFloat(result).toFixed(2)}$</div>
+        </div>
+        <div className="chart">
+          <Chart data={data} axes={axes} />
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from "react";
+
+export default function Form(props) {
+  const [values, setValues] = useState({
+    currentPrice: 2900,
+    strikeDate: "2020-06-19",
+    strikePrice: 2100,
+    type: "call",
+    volatility: 50
+  });
+  useEffect(() => {
+    calculate(null, values);
+  }, [values]);
+
+  function updateValue(e) {
+    const target = e.target;
+    setValues(prevState => {
+      prevState[target.name] = target.value;
+      return prevState;
+    });
+    calculate(e, values);
+  }
+  const setCall = e => {
+    console.log("clicked");
+    let newArr = { ...values };
+    newArr.type = "call";
+    setValues(newArr);
+    return;
+  };
+  const setPut = e => {
+    console.log("clicked");
+    let newArr = { ...values };
+    newArr.type = "put";
+    setValues(newArr);
+    return;
+  };
+  const { calculate } = props;
+  return (
+    <form id="calcForm" onSubmit={e => calculate(e, values)}>
+      <label>
+        Current Price:{" "}
+        <input
+          onChange={e => updateValue(e)}
+          value={values.currentPrice}
+          type="number"
+          name="currentPrice"
+        />
+      </label>
+      <label>
+        Strike Price
+        <input
+          onChange={e => updateValue(e)}
+          value={values.strikePrice}
+          type="number"
+          name="strikePrice"
+        />
+      </label>
+      <label>
+        Volatility
+        <input
+          onChange={e => updateValue(e)}
+          value={values.volatility}
+          type="number"
+          name="volatility"
+        />
+      </label>
+      <label>
+        Expire Date
+        <input
+          onChange={e => updateValue(e)}
+          value={values.strikeDate}
+          type="date"
+          name="strikeDate"
+        />
+      </label>
+      <label>
+        Call:{" "}
+        <input
+          onChange={e => setCall(e)}
+          type="radio"
+          name="oType"
+          value="call"
+          checked={values.type === "call" ? true : false}
+        />
+        Put:{" "}
+        <input
+          onChange={e => setPut(e)}
+          type="radio"
+          name="oType"
+          value="put"
+          checked={values.type === "put" ? true : false}
+        />
+      </label>
+
+      <input type="submit" value="Calculate" />
+    </form>
+  );
+}
